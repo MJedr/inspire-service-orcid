d7baaadd0e795a23bd8f0b5b8bf817e66ed59795
puntonim <puntonim@gmail.com>
2018-07-24 14:43:13 +0200

    Add pytest-vcr and first call: get_all_works_summary

--- inspire_services/orcid/conf.py
+++ inspire_services/orcid/conf.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from pkgsettings import Settings
 
 settings = Settings()
@@ -5,4 +6,5 @@ settings.configure(
     DO_USE_SANDBOX=False,
     CONSUMER_KEY='myorcidappkey',
     CONSUMER_SECRET='myorcidappsecret',
+    REQUEST_TIMEOUT=30,
 )
