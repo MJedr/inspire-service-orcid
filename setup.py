b2b9f35208733268ec7bd709101afe4e82973236
puntonim <puntonim@gmail.com>
2018-10-26 19:02:53 +0200

    3.2.1 release

--- setup.py
+++ setup.py
@@ -20,5 +20,5 @@ setup(**{'author': 'puntonim',
  'packages': ['inspire_service_orcid'],
  'tests_require': ['tox'],
  'url': 'https://github.com/puntonim/inspire-service-orcid',
- 'version': '3.2.0',
+ 'version': '3.2.1',
  'zip_safe': False})
