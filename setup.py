11ee45dd5b3664fe075766c968927e7b7a843b01
puntonim <puntonim@gmail.com>
2018-12-06 15:05:17 +0100

    8.0.0 released

--- setup.py
+++ setup.py
@@ -22,5 +22,5 @@ setup(**{'author': 'puntonim',
  'packages': ['inspire_service_orcid'],
  'tests_require': ['tox'],
  'url': 'https://github.com/puntonim/inspire-service-orcid',
- 'version': u'7.0.1',
+ 'version': u'8.0.0',
  'zip_safe': False})
